206fb6153045d20850328377589b96e8aa8cd0c3
Svein Halvor Halvorsen <svein@finkoslo.no>
2017-02-20 13:04:00 +0100

    Added POST /slack endpoint for slack-bot

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
 const app = require('express')();
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const getFact = require('./facts');
 
+app.use(bodyParser.urlencoded());
 app.use(logger('dev'));
 
 app.get('/', (req, res, next) => res.json({ fact: getFact() }));
+app.post('/slack', (req, res, next) => res.send(getFact()));
 
 const server = app.listen(process.env.PORT || 8888, function () {
   const host = server.address().address;
