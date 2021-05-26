2c41e671edbeff64842588f52837ba72aa9481cf
Svein Halvor Halvorsen <svein@fink.no>
2021-05-26 12:57:07 +0200

    Added group command

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const app = require('express')();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const contentful = require('./contenful');
 const getFact = require('./facts');
@@ -16,42 +17,64 @@ function factObject(extra) {
 
 const getEmplyees = role => contentful
   .fetchContent('finksEmployees')
-  .then(res => res[0].fields.finkEmployee
-    .map(empl => empl.fields)
-    .filter(empl => empl.position.includes(role))
-    .map(empl => ({
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `*${empl.name}* (${empl.email})\n${empl.keyWords}`
-      },
-      accessory: {
-        type: "image",
-        image_url: `https:${empl.largeProfilePicture 
-          && empl.largeProfilePicture.fields.file.url
-          || profileFallback}`,
-        alt_text: `${empl.name}`
-      }
-    }))
-  );
+  .then(res => res[0].fields.finkEmployee.map(empl => empl.fields))
+  .then(employees => employees.filter(empl => !!role ? empl.position.includes(role) : true));
+
+const getEmplyeesBlocks = role => getEmplyees(role)
+  .then(employees => employees.map(empl => ({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: `*${empl.name}* (${empl.email})\n${empl.keyWords}`
+    },
+    accessory: {
+      type: "image",
+      image_url: `https:${empl.largeProfilePicture 
+        && empl.largeProfilePicture.fields.file.url
+        || profileFallback}`,
+      alt_text: `${empl.name}`
+    }
+  })));
 
 app.use(bodyParser.urlencoded());
 app.use(logger('dev'));
 
 app.post('/slack', async (req, res, next) => {
-  let cmd = req.body.text;
+  let args = req.body.text.split(" ");
+  let cmd = args[0];
+  let arg = args[1];
+  
   switch (cmd) {
-    case 'devs':
-      return getEmplyees("Utvikler")
+    case 'dev':
+      return getEmplyeesBlocks("Utvikler")
         .then(employees => res.send({ blocks: employees }))
         .catch(error => res.send(`error: ${error}`));
-    case 'designers':
-      return getEmplyees("Designer")
+    
+    case 'design':
+      return getEmplyeesBlocks("Designer")
         .then(employees => res.send({ blocks: employees }))
         .catch(error => res.send(`error: ${error}`));
+    
+    case 'group':
+    case 'gruppe':
+      return getEmplyees()
+        .then(employees => employees.map(empl => empl.name))
+        .then(names => _.shuffle(names))
+        .then(randomNames => _.chunk(randomNames, arg || 4))
+        .then(groups => groups.map((group, i) => ({
+          type: "section", 
+          text: {
+            type: "mrkdwn",
+            text: `*Gruppe ${i}:* ${group.join(", ")}`
+          }
+        })))
+        .then(result => res.send({ blocks: result }))
+        .catch(error => res.send(`error: ${error}`));
+
     case 'fact':
     case 'fakta':
       return res.json(factObject({ response_type: 'in_channel' }));
+    
     default:
       console.warn(`Unrecognized command ${cmd}`);
       return res.send('Try `/fink fact`');
